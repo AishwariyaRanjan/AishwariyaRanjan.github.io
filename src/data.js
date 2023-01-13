7cbbad21abf95313a91ddc418ccf8d03b98b0880
AishwariyaRanjan <ranjanaishwariya107@gmail.com>
2023-01-13 16:32:52 -0700

    updated projects section

--- src/data.js
+++ src/data.js
@@ -1,26 +1,27 @@
 export const projects = [
+    
     {
-      title: "Coming Soon",
-      subtitle: "",
+      title: "DayDule",
+      subtitle: "Figma + Next.js + TailwindCSS + Node.js + Supabase" ,
       description:
-        "",
-      image: "./wip.png",
+        "Designed the landing page for ShadyForge, an event planning software startup using Figma. Currently working to program the page using Next.js and TailwindCSS on the frontend while developing the backend using Node.js and Supabase",
+      image: "./Daydule_landing.png",
       link: "",
     },
     {
-      title: "Coming Soon",
-      subtitle: "" ,
+      title: "Template Landing Page",
+      subtitle: "Next.js + TailwindCSS",
       description:
-        "",
-      image: "./wip.png",
+        "Learnt and independently completed front-end development of a sample landing page by converting a figma prototype into code in record time using NextJS, TailwindCSS and Github as a part of my senior year capstone project at Arizona State University.",    
+      image: "./ShadyForge_landing.png",
       link: "",
     },
     {
-      title: "Coming Soon",
-      subtitle: "" ,
+      title: "StressManager",
+      subtitle: "Figma + React + Node.js" ,
       description:
-        "",
-      image: "./wip.png",
+      "Presently redesigning and looking to develop a website to track heart rate and stress data collected from Fitbit for a Research Project at Arizona State University using Figma, React.js and Node.js for development and AWS S3, API Gateway, Lambda and DynamoDB for deployment.",
+      image: "./stressManager.png",
       link: "",
     },
     {
@@ -34,6 +35,6 @@ export const projects = [
   ];
 
   export const skills =[
-   "Python", "Java", "C/C++", "HTML5", "CSS3","JavaScript", "React" , "GitHub", "Kotlin", "Android Studios", "Figma", "Axure" 
+   "Python", "Java", "C/C++", "HTML5", "CSS3","JavaScript", "React.js", "Next.js" , "Amazon S3", "AWS API Gateway", "GitHub", "Kotlin", "Android Studios", "Figma", "Axure" 
 
   ];
